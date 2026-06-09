7957fcdce10df04b192be6341765baf597245714
Immelancholy <lenalowes0@gmail.com>
2026-06-09 18:01:02 +0100

    package: add meta

--- nix/package.nix
+++ nix/package.nix
@@ -1,8 +1,20 @@
-{ buildGoApplication, go }:
+{
+  buildGoApplication,
+  lib,
+  go,
+}:
 buildGoApplication {
   inherit go;
   pname = "linktui";
   version = "git";
   src = ../.;
   modules = ../govendor.toml;
+
+  meta = with lib; {
+    description = "Tui based wifi, bluetooth and vpn manager for linux";
+    homepage = "https://github.com/austinemk/linktui";
+    license = licenses.mit;
+    maintainers = with maintainers; [ Immelancholy ];
+    mainProgram = "linktui";
+  };
 }
